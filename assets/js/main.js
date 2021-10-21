62bf5961e89ed24b38e4bb1099ff496cb64d684c
yokoyamakazu <77267316+yokoyamakazu@users.noreply.github.com>
2021-10-22 04:28:55 +0900

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+$(function() {
+    var topBtn = $('#page-top');
+    topBtn.hide();
+    //スクロールが↓6行目指定数字に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
+
